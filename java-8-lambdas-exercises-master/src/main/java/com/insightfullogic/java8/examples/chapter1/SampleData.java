28288b3ff7d9ca823d94b6a77438ba500030ac9b
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-10-10 00:44:50 +0300

    done refactoring

--- java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter1/SampleData.java
+++ java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter1/SampleData.java
@@ -8,25 +8,24 @@ import static java.util.Arrays.asList;
 
 public class SampleData {
 
+    // artists
     public static final Artist johnColtrane = new Artist("John Coltrane", "US");
-
     public static final Artist johnLennon = new Artist("John Lennon", "UK");
     public static final Artist paulMcCartney = new Artist("Paul McCartney", "UK");
     public static final Artist georgeHarrison = new Artist("George Harrison", "UK");
     public static final Artist ringoStarr = new Artist("Ringo Starr", "UK");
-
     public static final List<Artist> membersOfTheBeatles = Arrays.asList(johnLennon, paulMcCartney, georgeHarrison, ringoStarr);
-
     public static final Artist theBeatles = new Artist("The Beatles", membersOfTheBeatles, "UK");
 
-    public static final Album aLoveSupreme = new Album("A Love Supreme", asList(new Track("Acknowledgement", 467), new Track("Resolution", 442)), asList(johnColtrane));
-
+    // albums
+    public static final Album aLoveSupreme = new Album("A Love Supreme",
+            asList(new Track("Acknowledgement", 467), new Track("Resolution", 442), new Track("short track", 30)),
+            asList(johnColtrane));
     public static final Album sampleShortAlbum = new Album("sample Short Album", asList(new Track("short track", 30)), asList(johnColtrane));
-
     public static final Album manyTrackAlbum = new Album("sample Short Album", asList(new Track("short track", 30), new Track("short track 2", 30), new Track("short track 3", 30), new Track("short track 4", 30), new Track("short track 5", 30)), asList(johnColtrane));
 
+    // streams
     public static Stream<Album> albums = Stream.of(aLoveSupreme);
-
     public static Stream<Artist> threeArtists() {
         return Stream.of(johnColtrane, johnLennon, theBeatles);
     }
--- java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter3/Refactor.java
+++ java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter3/Refactor.java
@@ -1,12 +1,15 @@
 package com.insightfullogic.java8.examples.chapter3;
 
 import com.insightfullogic.java8.examples.chapter1.Album;
+import com.insightfullogic.java8.examples.chapter1.SampleData;
 import com.insightfullogic.java8.examples.chapter1.Track;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static java.util.Arrays.asList;
+import static java.util.Arrays.sort;
 import static java.util.stream.Collectors.toSet;
 
 public class Refactor {
@@ -16,83 +19,135 @@ public class Refactor {
     }
 
     public static class Step0 implements LongTrackFinder {
-// BEGIN findLongTracks_0
-public Set<String> findLongTracks(List<Album> albums) {
-    Set<String> trackNames = new HashSet<>();
-    for(Album album : albums) {
-        for (Track track : album.getTrackList()) {
-            if (track.getLength() > 60) {
-                String name = track.getName();
-                trackNames.add(name);
+        // BEGIN findLongTracks_0
+        public Set<String> findLongTracks(List<Album> albums) {
+            Set<String> trackNames = new HashSet<>();
+            for (Album album : albums) {
+                for (Track track : album.getTrackList()) {
+                    if (track.getLength() > 60) {
+                        String name = track.getName();
+                        trackNames.add(name);
+                    }
+                }
             }
+            return trackNames;
         }
+        // END findLongTracks_0
     }
-    return trackNames;
-}
-// END findLongTracks_0
-    }
-
     public static class Step1 implements LongTrackFinder {
-// BEGIN findLongTracks_1
-public Set<String> findLongTracks(List<Album> albums) {
-    Set<String> trackNames = new HashSet<>();
-    albums.stream()
-          .forEach(album -> {
-              album.getTracks()
-                   .forEach(track -> {
-                       if (track.getLength() > 60) {
-                           String name = track.getName();
-                           trackNames.add(name);
-                       }
-                   });
-          });
-    return trackNames;
-}
-// END findLongTracks_1
+        // BEGIN findLongTracks_1
+        public Set<String> findLongTracks(List<Album> albums) {
+            Set<String> trackNames = new HashSet<>();
+            albums.stream()
+                    .forEach(album -> {
+                        album.getTracks()
+                                .forEach(track -> {
+                                    if (track.getLength() > 60) {
+                                        trackNames.add(track.getName());
+                                    }
+                                });
+                    });
+            return trackNames;
+        }
+        // END findLongTracks_1
     }
-
     public static class Step2 implements LongTrackFinder {
-// BEGIN findLongTracks_2
-public Set<String> findLongTracks(List<Album> albums) {
-    Set<String> trackNames = new HashSet<>();
-    albums.stream()
-          .forEach(album -> {
-              album.getTracks()
-                   .filter(track -> track.getLength() > 60)
-                   .map(track -> track.getName())
-                   .forEach(name -> trackNames.add(name));
-          });
-    return trackNames;
-}
+        // BEGIN findLongTracks_2
+        public Set<String> findLongTracks(List<Album> albums) {
+            Set<String> trackNames = new HashSet<>();
+            albums.stream()
+                    .forEach(album -> {
+                        album.getTracks()
+                                .filter(track -> track.getLength() > 60)
+                                .map(track -> track.getName())
+                                .forEach(name -> trackNames.add(name));
+                    });
+            return trackNames;
+        }
 // END findLongTracks_2
     }
-
     public static class Step3 implements LongTrackFinder {
-// BEGIN findLongTracks_3
-public Set<String> findLongTracks(List<Album> albums) {
-    Set<String> trackNames = new HashSet<>();
+        // BEGIN findLongTracks_3
+        public Set<String> findLongTracks(List<Album> albums) {
+            Set<String> trackNames = new HashSet<>();
 
-    albums.stream()
-          .flatMap(album -> album.getTracks())
-          .filter(track -> track.getLength() > 60)
-          .map(track -> track.getName())
-          .forEach(name -> trackNames.add(name));
+            albums.stream()
+                    .flatMap(album -> album.getTracks())
+                    .filter(track -> track.getLength() > 60)
+                    .map(track -> track.getName())
+                    .forEach(name -> trackNames.add(name));
 
-    return trackNames;
-}
+            return trackNames;
+        }
 // END findLongTracks_3
     }
-
     public static class Step4 implements LongTrackFinder {
-// BEGIN findLongTracks_4
-public Set<String> findLongTracks(List<Album> albums) {
-    return albums.stream()
-                 .flatMap(album -> album.getTracks())
-                 .filter(track -> track.getLength() > 60)
-                 .map(track -> track.getName())
-                 .collect(toSet());
-}
+        // BEGIN findLongTracks_4
+        public Set<String> findLongTracks(List<Album> albums) {
+            return albums.stream()
+                    .flatMap(album -> album.getTracks())
+                    .filter(track -> track.getLength() > 60)
+                    .map(track -> track.getName())
+                    .collect(toSet());
+        }
 // END findLongTracks_4
     }
 
+    public static void main(String[] args) {
+//        Step1 step1 = new Step1();
+//        System.out.println(step1.findLongTracks(asList(SampleData.aLoveSupreme, SampleData.sampleShortAlbum)));
+
+//        Step2 step2 = new Step2();
+//        System.out.println(step2.findLongTracks(asList(SampleData.aLoveSupreme, SampleData.sampleShortAlbum)));
+
+//        Step3 step3 = new Step3();
+//        System.out.println(step3.findLongTracks(asList(SampleData.aLoveSupreme, SampleData.sampleShortAlbum)));
+
+        Step4 step4 = new Step4();
+        System.out.println(step4.findLongTracks(asList(SampleData.aLoveSupreme, SampleData.sampleShortAlbum)));
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
