59c9f21887719e5dc578d99beab7c2b00df380de
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-10-27 22:53:25 +0300

    done Extract

--- workspace/ps2-tweets/src/twitter/Extract.java
+++ workspace/ps2-tweets/src/twitter/Extract.java
@@ -1,7 +1,15 @@
 package twitter;
 
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * Extract consists of methods that extract information from a list of tweets.
@@ -21,7 +29,18 @@ public class Extract {
      *         every tweet in the list.
      */
     public static Timespan getTimespan(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        
+        Instant start = tweets.stream()
+                               .min(Comparator.comparing(tweet -> tweet.getTimestamp()))
+                               .get()
+                               .getTimestamp();
+        
+        Instant end = tweets.stream()
+                            .max(Comparator.comparing(tweet -> tweet.getTimestamp()))
+                            .get()
+                            .getTimestamp();
+        
+        return new Timespan(start, end);
     }
 
     /**
@@ -40,11 +59,56 @@ public class Extract {
      *         include a username at most once.
      */
     public static Set<String> getMentionedUsers(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        
+        Set<String> mentions = new HashSet<>();
+        
+        // we use '+' because length(username) > 0
+        // \w is predefined class for [a-zA-Z_0-9]
+        Pattern up = Pattern.compile("(?:[^\\w-]|^)@([\\w-]+)");
+        
+        for (Tweet tweet : tweets) {
+            Matcher um = up.matcher(tweet.getText());
+            while(um.find()) {
+                mentions.add(um.group(1).toLowerCase());
+            }
+        }
+
+        return mentions;
     }
 
     /* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
      * Redistribution of original or derived work requires explicit permission.
      * Don't post any of this code on the web or to a public Github repository.
      */
+    
+    public static void main(String[] args) {
+        
+        Tweet t1 = new Tweet(1, "", "abcd @user1 def @user2 ghjk", Instant.now());
+        Tweet t2 = new Tweet(2, "", "@user3 def @user4", Instant.now());
+        Tweet t3 = new Tweet(3, "", "abc@user5 def abc.@user6.edu", Instant.now());
+
+        System.out.println(getMentionedUsers(Arrays.asList(t1, t2, t3)));
+        
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- workspace/ps2-tweets/test/twitter/ExtractTest.java
+++ workspace/ps2-tweets/test/twitter/ExtractTest.java
@@ -4,15 +4,29 @@ import static org.junit.Assert.*;
 
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ExtractTest {
 
     /*
-     * TODO: your testing strategies for these methods should go here.
-     * Make sure you have partitions.
+     * Testing strategy.
+     * 
+     * Partitions:
+     * 
+     *  - no usernames, 1 or 2 usernames
+     *  - contains _, -; contains not legal characters 
+     *  - username in the beginning or end of the string
+     *  - username is preceded by a legal character, so it's not a mention: bitdiddle@mit.edu
+     *    but this is a mention of @mit: @mit.edu
+     *  - usernames are case insensitive
+     *  - we return set of unique usernames, so string should contain repetitions 
+     *  - length of username == 0 (not legal), 1, > 1   
+     * 
+     * 
      */
     
     private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
@@ -20,6 +34,14 @@ public class ExtractTest {
     
     private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
     private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "rivest talk in 30 minutes #hype", d2);
+    
+    // tweets to test mentions
+    // beginning or end of the string, case insensitive, unique usernames
+    private static final Tweet tweet3 = new Tweet(3, "bbitdiddle", "@user1 #hype @usER1 @uSeR2", d1);
+    // length of username > 0, contains _, -
+    private static final Tweet tweet4 = new Tweet(4, "bbitdiddle", "abc @user89_- def @USer0_1-2 ght @_ htr @ #hype", d1);
+    // not legal characters
+    private static final Tweet tweet5 = new Tweet(5, "bbitdiddle", "@us&er def @user1& #hype @user2& bitdiddle@mit1.edu dfdfd @mit2.edu", d1);
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
@@ -34,13 +56,36 @@ public class ExtractTest {
         assertEquals("expected end", d2, timespan.getEnd());
     }
     
+  
     @Test
     public void testGetMentionedUsersNoMention() {
         Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1));
-        
+       
         assertTrue("expected empty set", mentionedUsers.isEmpty());
     }
+    
+    @Test
+    public void getMentionedUserBeginnigEndString() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet3));
+        
+        assertEquals(new HashSet<String>(Arrays.asList("user1", "user2")), mentionedUsers);
+    }
 
+    @Test
+    public void getMentionedUserLengthSpecialSymbols() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet4));
+        
+        assertEquals(new HashSet<String>(Arrays.asList("user89_-", "user0_1-2", "_")), mentionedUsers);
+    }
+    
+    @Test
+    public void getMentionedUserBeginnigNotLegalSymbols() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet5));
+        
+        assertEquals(new HashSet<String>(Arrays.asList("us", "user1", "user2", "mit2")), mentionedUsers);
+    }
+    
+    
     /*
      * Warning: all the tests you write here must be runnable against any
      * Extract class that follows the spec. It will be run against several staff
