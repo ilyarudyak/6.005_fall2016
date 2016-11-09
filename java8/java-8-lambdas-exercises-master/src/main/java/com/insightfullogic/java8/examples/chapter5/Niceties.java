255139fe7f8a2f641a34c045efe0b46a862000f2
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-11-10 00:20:50 +0300

    done niceties

--- java8/java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter5/Niceties.java
+++ java8/java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter5/Niceties.java
@@ -2,72 +2,127 @@ package com.insightfullogic.java8.examples.chapter5;
 
 import com.insightfullogic.java8.examples.chapter1.Album;
 import com.insightfullogic.java8.examples.chapter1.Artist;
+import com.insightfullogic.java8.examples.chapter1.SampleData;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Niceties {
 
-    abstract class ArtistService {
+    abstract static class ArtistService {
 
         protected Map<String, Artist> artistCache = new HashMap<>();
 
         public abstract Artist getArtist(String name);
 
         protected Artist readArtistFromDB(String name) {
+            fromDB();
             return new Artist(name, "UK");
+        }
+
+        protected void fromDB() {
+            System.out.println("read from d/b ...");
+        }
+
+        protected void fromCache() {
+            System.out.println("read from cache ...");
         }
     }
 
-    class OldArtistService extends ArtistService {
+    static class OldArtistService extends ArtistService {
         // BEGIN ARTIST_CACHE_OLD
-public Artist getArtist(String name) {
-    Artist artist = artistCache.get(name);
-    if (artist == null) {
-        artist = readArtistFromDB(name);
-        artistCache.put(name, artist);
-    }
-    return artist;
-}
+        public Artist getArtist(String name) {
+            Artist artist = artistCache.get(name);
+            if (artist == null) {
+                artist = readArtistFromDB(name);
+                artistCache.put(name, artist);
+            } else {
+                fromCache();
+            }
+            return artist;
+        }
         // END ARTIST_CACHE_OLD
     }
 
-    class Java8ArtistService extends ArtistService {
+    static class Java8ArtistService extends ArtistService {
+
         // BEGIN ARTIST_CACHE_COMPUTE
-public Artist getArtist(String name) {
-    return artistCache.computeIfAbsent(name, this::readArtistFromDB);
-}
+        public Artist getArtist(String name) {
+
+            if (artistCache.get(name) != null) {
+                fromCache();
+            }
+            return artistCache
+                    .computeIfAbsent(name, this::readArtistFromDB);
+        }
         // END ARTIST_CACHE_COMPUTE
     }
 
 
-    class ImperativeCount {
+    static class ImperativeCount {
 
         public Map<Artist, Integer> countAlbums(Map<Artist, List<Album>> albumsByArtist) {
             // BEGIN COUNT_ALBUMS_VALUES_UGLY
-Map<Artist, Integer>  countOfAlbums = new HashMap<>();
-for(Map.Entry<Artist, List<Album>> entry : albumsByArtist.entrySet()) {
-    Artist artist = entry.getKey();
-    List<Album> albums = entry.getValue();
-    countOfAlbums.put(artist, albums.size());
-}
+            Map<Artist, Integer> countOfAlbums = new HashMap<>();
+            for (Map.Entry<Artist, List<Album>> entry : albumsByArtist.entrySet()) {
+                Artist artist = entry.getKey();
+                List<Album> albums = entry.getValue();
+                countOfAlbums.put(artist, albums.size());
+            }
             // END COUNT_ALBUMS_VALUES_UGLY
             return countOfAlbums;
         }
     }
 
-    class Java8Count {
+    static class Java8Count {
         public Map<Artist, Integer> countAlbums(Map<Artist, List<Album>> albumsByArtist) {
             // BEGIN COUNT_ALBUMS_VALUES_FOREACH
-Map<Artist, Integer>  countOfAlbums = new HashMap<>();
-albumsByArtist.forEach((artist, albums) -> {
-    countOfAlbums.put(artist, albums.size());
-});
+            Map<Artist, Integer> countOfAlbums = new HashMap<>();
+            albumsByArtist.forEach((artist, albums) -> countOfAlbums.put(artist, albums.size()));
             // END COUNT_ALBUMS_VALUES_FOREACH
             return countOfAlbums;
         }
     }
 
+    public static void main(String[] args) {
+
+//        OldArtistService oas = new OldArtistService();
+//        oas.getArtist("Madonna");
+//        oas.getArtist("Lady Gaga");
+//        oas.getArtist("Madonna");
+//
+//        System.out.println();
+//        Java8ArtistService j8as = new Java8ArtistService();
+//        j8as.getArtist("Madonna");
+//        j8as.getArtist("Lady Gaga");
+//        j8as.getArtist("Madonna");
+
+        ImperativeCount ic = new ImperativeCount();
+        Map<Artist, List<Album>> albumsByArtist = new HashMap<>();
+        albumsByArtist.put(SampleData.johnColtrane, SampleData.albums.collect(Collectors.toList()));
+        System.out.println(ic.countAlbums(albumsByArtist));
+
+        Java8Count j8c = new Java8Count();
+        System.out.println();
+        System.out.println(j8c.countAlbums(albumsByArtist));
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
