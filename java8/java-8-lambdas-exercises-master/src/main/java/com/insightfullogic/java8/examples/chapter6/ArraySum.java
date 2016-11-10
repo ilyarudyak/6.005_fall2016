7ea1249eed204a441fa65015dbd5e0e3f20be152
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-11-10 23:53:21 +0300

    done timing

--- java8/java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter6/ArraySum.java
+++ java8/java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter6/ArraySum.java
@@ -4,60 +4,101 @@ import com.insightfullogic.java8.examples.chapter1.Album;
 import com.insightfullogic.java8.examples.chapter1.SampleData;
 import com.insightfullogic.java8.examples.chapter1.Track;
 
-import org.openjdk.jmh.Main;
-import org.openjdk.jmh.annotations.*;
+
 import org.openjdk.jmh.runner.RunnerException;
 
+
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
-@State(Scope.Benchmark)
-@BenchmarkMode(Mode.AverageTime)
+
 public class ArraySum {
 
-    public static void main(String[] ignore) throws IOException, RunnerException {
-        final String[] args = {
-            ".*ArraySum.*",
-            "-wi",
-            "5",
-            "-i",
-            "5"
-        };
-        Main.main(args);
-    }
+    public static final int SMALL_SIZE = 10;
+    public static final int MEDIUM_SIZE = 100;
+    public static final int LARGE_SIZE = 10000;
+
+    public enum StreamType { SERIAL, PARALLEL };
+
 
     public List<Album> albums;
 
-    @Setup
     public void initAlbums() {
-        int n = Integer.getInteger("arraysum.size", 1000);
-        albums = IntStream.range(0, n)
-                          .mapToObj(i -> SampleData.aLoveSupreme.copy())
-                          .collect(toList());
+        albums = IntStream.range(0, LARGE_SIZE)
+                .mapToObj(i -> SampleData.aLoveSupreme.copy())
+                .collect(toList());
     }
 
-    @GenerateMicroBenchmark
     // BEGIN serial
-public int serialArraySum() {
-    return albums.stream()
-                 .flatMap(Album::getTracks)
-                 .mapToInt(Track::getLength)
-                 .sum();
-}
+    public int serialArraySum() {
+        return albums.stream()
+                .flatMap(Album::getTracks)
+                .mapToInt(Track::getLength)
+                .sum();
+    }
     // END serial
 
-    @GenerateMicroBenchmark
     // BEGIN parallel
-public int parallelArraySum() {
-    return albums.parallelStream()
-                 .flatMap(Album::getTracks)
-                 .mapToInt(Track::getLength)
-                 .sum();
-}
+    public int parallelArraySum() {
+        return albums.parallelStream()
+                .flatMap(Album::getTracks)
+                .mapToInt(Track::getLength)
+                .sum();
+    }
     // END parallel
-    
+
+    private void time(StreamType type) {
+
+        Instant start = Instant.now();
+
+        switch (type) {
+            case SERIAL:
+                serialArraySum();
+                break;
+            case PARALLEL:
+                parallelArraySum();
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+
+        Instant end = Instant.now();
+        System.out.println(type + ":" + Duration.between(start, end).getNano());
+    }
+
+
+    public static void main(String[] ignore) throws IOException, RunnerException {
+
+        ArraySum as = new ArraySum();
+        as.initAlbums();
+        as.time(StreamType.SERIAL);
+        as.time(StreamType.PARALLEL);
+
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
