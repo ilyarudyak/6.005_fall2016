0c076668b1caa7a55c36610e47eb57c989837495
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-11-10 16:01:50 +0300

    done name count

--- java8/java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/exercises/chapter5/WordCount.java
+++ java8/java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/exercises/chapter5/WordCount.java
@@ -2,7 +2,9 @@ package com.insightfullogic.java8.exercises.chapter5;
 
 import com.insightfullogic.java8.exercises.Exercises;
 
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,6 +15,19 @@ public class WordCount {
 
     public static Map<String, Long> countWords(Stream<String> names) {
         return Exercises.replaceThisWithSolution();
+    }
+
+    public static void main(String[] args) {
+
+        Stream<String> names = Stream.of("John", "Paul", "George", "John",
+                "Paul", "John");
+
+        Map<String, Long> namesMap = names.collect(
+                Collectors.groupingBy(Function.identity(), Collectors.counting())
+        );
+
+        System.out.println(namesMap);
+
     }
 
 }
