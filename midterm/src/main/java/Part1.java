43c05b1f7b3e4e32ec48413bdd4b12cc57dda3d1
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-12-15 15:04:42 +0300

    done part 1

---
+++ midterm/src/main/java/Part1.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+/**
+ * Created by ilyarudyak on 12/15/16.
+ */
+public class Part1 {
+
+    public static void main(String[] args) {
+
+//        double oneThird = 1/3;
+//        System.out.println(oneThird);
+
+//        int sum = 0;
+//        int n = 0;
+//        int average = sum/n;
+//        System.out.println(average);
+
+        String[] strings = {"firstStr", "secondStr"};
+        System.out.println(Arrays.toString(strings));
+
+        // error
+//        List[int] l = Arrays.asList(1, 2, 3);
+    }
+}
