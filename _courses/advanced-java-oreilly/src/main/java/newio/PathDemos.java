2afd69027f125183ce540bcfca5fb090525c2552
Ilya Rudyak <ilyarudyak@yahoo.com>
2017-04-03 14:29:19 +0300

    done with path demo

--- _courses/advanced-java-oreilly/src/main/java/newio/PathDemos.java
+++ _courses/advanced-java-oreilly/src/main/java/newio/PathDemos.java
@@ -5,12 +5,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PathDemos {
-    public static void main(String[] args) {
-        // Using Paths.get(...) to create a Path
-        Path dictionary = Paths.get("/", "usr", "share", "dict", "web2");
-        System.out.println(dictionary);
 
-        Path home = Paths.get("/Users/kousen");
+    public static void mainMethods() {
+        // Using Paths.get(...) to create a Path
+        Path data = Paths.get("/", "src", "main", "resources", "newio");
+        System.out.println(data);
+
+        Path home = Paths.get("/Users/ilyarudyak");
         System.out.println(home);
 
         // Using resolve to find nested paths
@@ -21,25 +22,65 @@ public class PathDemos {
         Path downloads = docs.resolveSibling("Downloads");
         System.out.println(downloads);
 
+        Path relative = downloads.relativize(docs.resolve("IMG_0004.PNG"));
+        System.out.println(relative);
+    }
+
+    public static void normalize() {
+        // Normalize a path
+        Path p = Paths.get("/Users/ilyarudyak/Downloads/../Documents/IMG_0004.PNG").normalize();
+        System.out.println("Normalized: " + p);
+    }
+
+    public static void otherMethods() {
+
         // Project directory
         Path project = Paths.get(".");
         System.out.println(project);
         System.out.println(project.toAbsolutePath());
         System.out.println("As a URI: " + project.toUri());
 
-        // Normalize a path
-        Path p = Paths.get("/Users/kousen/Documents/./IntelliJ/..").normalize();
-        System.out.println("Normalized: " + p);
-
+        System.out.println("--------------------------");
         System.out.println("parent: " + project.toAbsolutePath().getParent());
         System.out.println("file name: " + project.toAbsolutePath().getFileName());
         System.out.println("root: " + project.toAbsolutePath().getRoot());
+
+        System.out.println("--------------------------");
         for (Path path : project.toAbsolutePath()) {
             System.out.println(path);
         }
 
+        System.out.println("--------------------------");
         File localDir = new File("..");
         System.out.println(localDir);
         System.out.println(localDir.toPath().toAbsolutePath().normalize());
     }
+
+    public static void main(String[] args) {
+//        mainMethods();
+//        normalize();
+        otherMethods();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
